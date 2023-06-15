bcfc8f926885da25d3acff25b55528a505fcc77e
Bart Massey <bart.massey@gmail.com>
2023-06-15 14:52:18 -0700

    added proper argument parsing, support for tracking grading/ dir

--- clone-projects.py
+++ clone-projects.py
@@ -1,10 +1,18 @@
 # Clone projects for grading.
 # Bart Massey 2022
 
-import csv, re, subprocess, sys, zipfile
+import argparse, csv, re, subprocess, sys, zipfile
 from pathlib import Path
 
-project_archive = zipfile.ZipFile(sys.argv[1])
+parser = argparse.ArgumentParser(
+    prog='clone-projects',
+    description='Clone a student project repo for grading',
+)
+parser.add_argument("--overwrite-grading", action="store_true")
+parser.add_argument("filename")
+args = parser.parse_args()
+
+project_archive = zipfile.ZipFile(args.filename)
 projects = []
 for zipinfo in project_archive.infolist():
     user = re.sub(r"_.*$", r"", zipinfo.filename)
@@ -22,38 +30,52 @@ for zipinfo in project_archive.infolist():
 f = open("failures.csv", "w")
 failures = csv.writer(f)
 
-dest = Path("staged")
-if not dest.is_dir():
-    dest.mkdir(mode=0o700)
+sdest = Path("staged")
+if not sdest.is_dir():
+    sdest.mkdir(mode=0o700)
+gdest = Path("graded")
+if not gdest.is_dir():
+    gdest.mkdir(mode=0o700)
 for user, link, slug, body in projects:
-    path = dest / slug
-    if path.is_dir():
-        continue
-    print(f'clone {link}: ', end="", flush=True)
-    url = re.sub(r"^http(s)?://", r"ssh://git@", link)
-    if not re.match(r"ssh://git@", url):
-        print("bad url: ", url)
-        continue
-    proc = subprocess.run(
-        ["git", "clone", url, path],
-        capture_output=True,
-        encoding="utf-8",
-    )
-    if proc.returncode == 0:
-        print("ok")
-    else:
-        print("failed")
-        print()
-        failures.writerow([slug, link, user])
-        for l in proc.stderr.splitlines():
-            if re.match("remote:.[A-Z]", l):
-                print(l.strip())
-        print()
-        print(body)
-        continue
+    spath = sdest / slug
+    has_spath = spath.is_dir()
+    gpath = gdest / slug
+    has_gpath = gpath.is_dir()
+    clone = False
+    path = spath
+    if has_gpath:
+        path = gpath
+    elif not has_spath:
+        clone = True
+    if clone:
+        print(f'clone {link}: ', end="", flush=True)
+        url = re.sub(r"^http(s)?://", r"ssh://git@", link)
+        if not re.match(r"ssh://git@", url):
+            print("bad url: ", url)
+            continue
+        proc = subprocess.run(
+            ["git", "clone", url, path],
+            capture_output=True,
+            encoding="utf-8",
+        )
+        if proc.returncode == 0:
+            print("ok")
+        else:
+            print("failed")
+            print()
+            failures.writerow([slug, link, user])
+            for l in proc.stderr.splitlines():
+                if re.match("remote:.[A-Z]", l):
+                    print(l.strip())
+            print()
+            print(body)
+            continue
     
-    with open(path / "GRADING.txt", "w") as gr:
-        print("Project Name", file=gr)
-        print("Member Names", file=gr)
-        print("-", file=gr)
-        print(file=gr)
+    grading = path / "GRADING.txt"
+    if not grading.is_file() or args.overwrite_grading:
+        with open(grading, "w") as gr:
+            print("Project Name", file=gr)
+            print("Member Names", file=gr)
+            print(link, file=gr)
+            print("-", file=gr)
+            print(file=gr)
