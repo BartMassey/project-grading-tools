54acdfce433dd8c77c9cbeacaeaf8f7de3d04413
Bart Massey <bart.massey@gmail.com>
2023-06-15 12:44:01 -0700

    adjusted cloner for Canvas workflow

--- clone-projects.py
+++ clone-projects.py
@@ -1,11 +1,23 @@
 # Clone projects for grading.
 # Bart Massey 2022
 
-import csv, re, subprocess, yaml
+import csv, re, subprocess, sys, zipfile
 from pathlib import Path
 
-with open("projects.yaml", "r") as p:
-    projects = yaml.safe_load(p)
+project_archive = zipfile.ZipFile(sys.argv[1])
+projects = []
+for zipinfo in project_archive.infolist():
+    user = re.sub(r"_.*$", r"", zipinfo.filename)
+    body = str(project_archive.read(zipinfo))
+    linkinfo = re.search(r'href="(https://git[^"/]*)/([^"/]+)/([^"/]+)(/[^"]*)?"', body)
+    if linkinfo:
+        linkage = [linkinfo[c] for c in [1, 2, 3]]
+        _, _, slug = linkage
+        link = '/'.join(linkage)
+        projects.append((user, link, slug, body))
+    else:
+        print("{user}: could not find link in body")
+        print(body)
 
 f = open("failures.csv", "w")
 failures = csv.writer(f)
@@ -13,15 +25,13 @@ failures = csv.writer(f)
 dest = Path("staged")
 if not dest.is_dir():
     dest.mkdir(mode=0o700)
-for p in projects:
-    members = [m["name"] for m in p["members"]]
-    member_names = ", ".join(members)
-    path = dest / p["slug"]
+for user, link, slug, body in projects:
+    path = dest / slug
     if path.is_dir():
         continue
-    print(f'clone {p["name"]}: ', end="")
-    url = re.sub("^http(s)?://", "ssh://git@", p["repo"])
-    if not re.match("ssh://git@", url):
+    print(f'clone {link}: ', end="", flush=True)
+    url = re.sub(r"^http(s)?://", r"ssh://git@", link)
+    if not re.match(r"ssh://git@", url):
         print("bad url: ", url)
         continue
     proc = subprocess.run(
@@ -33,16 +43,17 @@ for p in projects:
         print("ok")
     else:
         print("failed")
-        #people = [f'{m["name"]} <{m["email"]}>' for m in p["members"]]
-        #failures.writerow([p["slug"], p["repo"], ','.join(people)])
-        failures.writerow([p["slug"], p["repo"]])
+        print()
+        failures.writerow([slug, link, user])
         for l in proc.stderr.splitlines():
             if re.match("remote:.[A-Z]", l):
                 print(l.strip())
+        print()
+        print(body)
         continue
     
     with open(path / "GRADING.txt", "w") as gr:
-        print(p["name"], file=gr)
-        print(member_names, file=gr)
+        print("Project Name", file=gr)
+        print("Member Names", file=gr)
         print("-", file=gr)
         print(file=gr)
